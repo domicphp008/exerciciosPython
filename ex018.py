fbf1b4df0bf424c22930564918fbd32fa61aea65
Domicio Jose <testedomicio@hotmail.com>
2017-11-22 20:22:14 -0200

    Modificanfo o import do exercicio

--- ex018.py
+++ ex018.py
@@ -1,8 +1,8 @@
-import math
+from math import radians, sin, cos, tan
 angulo = float(input('Digite o angulo que você deseja: '))
-seno = math.sin(math.radians(angulo))
+seno = sin(radians(angulo))
 print('O angulo de {} tem o SENO de {:.2f}'.format(angulo, seno))
-cosseno = math.cos(math.radians(angulo))
+cosseno = cos(radians(angulo))
 print('O angulo de {} tem o COSSENO de{:.2f}'.format(angulo, cosseno))
-tangente = math.tan(math.radians(angulo))
+tangente = tan(radians(angulo))
 print('O angulo de {} tem a TANGETE de {:.2f}'.format(angulo, tangente))
